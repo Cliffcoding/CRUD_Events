ada72c5eb7beb995775cc5cc102069e4bb056143
Jason Clifton <j.clifton1473@gmail.com>
2017-06-13 15:33:01 -0600

    added production staging

--- knexfile.js
+++ knexfile.js
@@ -5,6 +5,10 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/jasons-events'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 
 };
